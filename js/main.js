e07973518ad85d360158817af5f5b0f6e673f762
aicholpondev <aicholponsultanii@gmail.com>
2023-11-29 15:08:43 +0600

    new section

--- js/main.js
+++ js/main.js
@@ -15,17 +15,38 @@ const swiper = new Swiper(".mySwiper", {
         clickable: true,
     },
     breakpoints: {
-        640: {
+        450:{slidesPerView:1,
+            spaceBetween:10,
+
+        },
+        992: {
             slidesPerView: 2,
             spaceBetween: 20,
         },
-        768: {
-            slidesPerView: 3,
+        1024: {
+            slidesPerView: 2.5,
             spaceBetween: 40,
         },
-        1024: {
+        1440: {
             slidesPerView: 3,
             spaceBetween: 50,
         },
     },
 });
+const btnMenu = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu-general');
+const btnOpen = document.querySelector('.menu-open');
+const btnClose = document.querySelector('.menu-close');
+
+btnMenu.addEventListener("click", () => {
+    if (menu.style.display === "none") {
+        menu.style.display = "block"
+        menu.style.display = "flex"
+        btnOpen.style.display = "none"
+        btnClose.style.display = "block"
+    } else {
+        menu.style.display = "none"
+        btnOpen.style.display = "block"
+        btnClose.style.display = "none"
+    }
+})
